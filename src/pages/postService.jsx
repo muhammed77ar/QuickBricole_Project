0173da89e9b10fe156158aa9fff0d70e5d161b02
Muhammad <mohamedar4477@gmail.com>
2024-01-14 20:33:13 +0100

    add the post project page

--- src/pages/postService.jsx
+++ src/pages/postService.jsx
@@ -1,5 +1,35 @@
+import { Container, InputContainer, Section1, Section2, Title } from "../styledComponent/postServiceStyle";
+
 export default function PostService(){
     return <>
-    <h3>Post a Service</h3>
+        <Container>
+            <Title>Post a need</Title>
+            <InputContainer>
+                <form>
+                    <div>
+                    <Section1>
+                        <label htmlFor="">Title of the need:</label>
+                        <input type="text" name="" id="title" placeholder="Enter your need"/>
+                        <label htmlFor="">Location:</label>
+                        <select id="location">
+                            <option value="Select your city">Select your location</option>
+                        </select>
+                        <label htmlFor="">Category:</label>
+                        <select id="category">
+                            <option value="Select a category">Select a Category</option>
+                        </select>
+                        <label htmlFor="">Price:</label>
+                        <input type="text" name="" id="price" placeholder="Enter a price"/>
+                        
+                    </Section1>
+                    <Section2>
+                        <label htmlFor="">Description:</label>
+                        <textarea placeholder="Enter a description"></textarea>
+                    </Section2>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </InputContainer>
+        </Container>
     </>
 }
---
+++ src/styledComponent/postServiceStyle.js
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+
+const mobileBreakPoint = "1100px"
+
+export const Container = styled.div`
+padding: 100px 0;
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 100%;
+`
+export const Title = styled.h1`
+margin: 30px 0;
+color: #1E88E5;
+`
+export const InputContainer = styled.div`
+  button {
+    align-self: center;
+    background-color: #1e88e5;
+    border-radius: 8.08px;
+    border: none;
+    color: white;
+    width: 392px;
+    height: 55.1px;
+    font-weight: 900;
+    font-size: 20px;
+    text-transform: capitalize;
+    box-shadow: 4.5px 4.5px 3.6px rgba(0, 0, 0, 0.031),
+      12.5px 12.5px 10px rgba(0, 0, 0, 0.045),
+      30.1px 30.1px 24.1px rgba(0, 0, 0, 0.059),
+      100px 100px 80px rgba(0, 0, 0, 0.09);
+    transition: all 0.3s ease;
+    &:hover {
+      transform: scale(0.96);
+    }
+    @media screen and (max-width:${mobileBreakPoint}){
+        margin-top: 10px;
+    }
+  }
+  
+  form {
+    @media screen and (max-width: ${mobileBreakPoint}) {
+        display: flex;
+        flex-direction: column;
+    }
+    div {
+      display: flex;
+      @media screen and (max-width: ${mobileBreakPoint}) {
+        flex-direction: column;
+      }
+    }
+  }
+`;
+export const Section1 = styled.div`
+display: flex;
+flex-direction: column;
+input{
+    background-color: #EEEEEE;
+    border-radius: 8.08px;
+    border: none;
+    width: 500px;
+    height: 55.65px;
+    padding: 0 10px;
+    margin-bottom: 15px;
+}
+select{
+    background-color: #EEEEEE;
+    border-radius: 8.08px;
+    border: none;
+    width: 500px;
+    height: 55.65px;
+    padding: 0 10px;
+    margin-bottom: 15px;
+}
+
+`
+export const Section2 = styled.div`
+display: flex;
+flex-direction: column;
+margin-left: 50px;
+  textarea {
+    width: 500px;
+    height: 337px;
+    background-color: #eeeeee;
+    border-radius: 8.08px;
+    border: none;
+    padding: 10px;
+  }
+  @media screen and (max-width:${mobileBreakPoint}) {
+    margin-left: 0;
+  }
+  
+`;
